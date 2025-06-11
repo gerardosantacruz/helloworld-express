daf0a2897825ca5a41f2d49cdbce7b4bca78a796
Gerardo Santa Cruz <gsanta@live.com>
2025-06-11 10:16:23 -0300

    update test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hola Mundo desde Express y Coolify 🚀</h1>');
+  res.send('<h1>Actualizado - Hola Mundo desde Express y Coolify 🚀</h1>');
 });
 
 const port = process.env.PORT || 3000;
