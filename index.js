c7c8820d3269523e33ea321e8320593e53da6d62
Gerardo Santa Cruz <gsanta@live.com>
2025-06-10 23:30:42 -0300

    App básica con Express

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hola Mundo desde Express y Coolify 🚀</h1>');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor Express escuchando en el puerto ${port}`);
+});
